4cc6e967ac1dc9452f49fd11d6993dfdf5326a2c
diego.juez <diego.juez@bridgestone.eu>
2015-08-03 10:39:18 +0200

    First upload

---
+++ MVC5Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Example.Startup))]
+namespace MVC5Example
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
